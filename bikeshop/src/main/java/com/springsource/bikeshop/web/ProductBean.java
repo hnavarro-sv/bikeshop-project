4308c7a477b13da85fa0dbd6b1e8296f7502dd15
Hugo Navarro <hugo.navarro.sv@gmail.com>
2011-12-28 14:02:19 -0600

    Version inicial

---
+++ bikeshop/src/main/java/com/springsource/bikeshop/web/ProductBean.java
@@ -0,0 +1,8 @@
+package com.springsource.bikeshop.web;
+
+import com.springsource.bikeshop.domain.Product;
+import org.springframework.roo.addon.jsf.managedbean.RooJsfManagedBean;
+
+@RooJsfManagedBean(entity = Product.class, beanName = "productBean")
+public class ProductBean {
+}
---
+++ bikeshop/src/main/java/com/springsource/bikeshop/web/SupplierBean.java
@@ -0,0 +1,8 @@
+package com.springsource.bikeshop.web;
+
+import com.springsource.bikeshop.domain.Supplier;
+import org.springframework.roo.addon.jsf.managedbean.RooJsfManagedBean;
+
+@RooJsfManagedBean(entity = Supplier.class, beanName = "supplierBean")
+public class SupplierBean {
+}
